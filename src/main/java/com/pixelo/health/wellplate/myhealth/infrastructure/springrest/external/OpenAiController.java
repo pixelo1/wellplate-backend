c08d38a3d3c2c9fa2664eabd63ae47e696fabbd4
pixelo1 <hoan1015@gmail.com>
2025-02-24 21:52:39 +0900

    chatgpt openai 연동

--- src/main/java/com/pixelo/health/wellplate/myhealth/infrastructure/springrest/external/OpenAiController.java
+++ src/main/java/com/pixelo/health/wellplate/myhealth/infrastructure/springrest/external/OpenAiController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/ai")
-public class GptController {
+public class OpenAiController {
 
     private final OpenAiChatModel openAiChatModel;
 
